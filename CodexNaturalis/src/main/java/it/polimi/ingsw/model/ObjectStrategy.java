f6664150425f4bea50fdab0f2fa5e4fd655de032
Marouan Ouadi <ouadimarouan@gmail.com>
2024-04-16 15:12:24 +0200

    Completed ObjectStrategy and ResourceStrategy

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectStrategy.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectStrategy.java
@@ -2,24 +2,28 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.enumeration.CornerObject;
 
-import java.util.ArrayList;
+import java.util.EnumMap;
 
-public class ObjectStrategy implements ObjectiveStrategy{
+public class ObjectStrategy implements ObjectiveStrategy {
 
-    private CornerObject ObjectStrategyToCheck;
-    private int NumOfCheck;
+    private final CornerObject objectStrategyToCheck;
+    private int numOfObjectsToCheck;
+
+    public ObjectStrategy(CornerObject objectStrategyToCheck1, int numOfObjectsToCheck) {
+        this.objectStrategyToCheck = objectStrategyToCheck1;
+        this.numOfObjectsToCheck = numOfObjectsToCheck;
+    }
 
     /**
      * this method will be recognized if the Resource cards
      * on the PlayerDesk meet the requirements of the objective card.
      * in this case it will analyze the objective that requires a specific sequence of objects
+     *
      * @param desk
      */
-    public void isSatisfied (PlayerDesk desk){
-
-        EnumMap<CornerObject, Integer>  TotaleObjects = desk.getTotalObjects();
-
-
+    public int isSatisfied(PlayerDesk desk) {
+        EnumMap<CornerObject, Integer> TotaleObjects = desk.getTotalObjects();
+        return TotaleObjects.get(objectStrategyToCheck) / numOfObjectsToCheck;
     }
 
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceStrategy.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceStrategy.java
@@ -2,34 +2,34 @@ package it.polimi.ingsw.model;
 
 import it.polimi.ingsw.model.enumeration.Resource;
 
-import java.util.ArrayList;
+import java.util.EnumMap;
 
-public class ResourceStrategy implements ObjectiveStrategy{
+public class ResourceStrategy implements ObjectiveStrategy {
 
-    private Resource ResourceStrategyToCheck;
-    private int NumOfResourceToCheck;
+    private final Resource resourceStrategyToCheck;
+    private final int numOfResourcesToCheck;
 
     /**
      * Constructor which assigns the Strategy that needs to be checked inside the class ResourceStrategy (i.g. The ObjcetiveCard requieres two elements of type insect)
-     * @param ResourceStrategyToCheck
+     *
+     * @param resourceStrategyToCheck
      */
 
-    public ResourceStrategy(ResourceStrategyValues ResourceStrategyToCheck)
-    {
-        this.ResourceStrategyToCheck = ResourceStrategyToCheck;
+    public ResourceStrategy(Resource resourceStrategyToCheck, int numOfResourcesToCheck) {
+        this.resourceStrategyToCheck = resourceStrategyToCheck;
+        this.numOfResourcesToCheck = numOfResourcesToCheck;
     }
-
-    //enum map è una mappa che contiene ha come chiave la risorsa mentre come valore il numero di risorse. Quello che bisogna fare è vedere quale è il tipo di risorsa
-    // della strategy da controllare(c'è l'attributo privato apposta) prendere dalla mappa il numero di volte e restituire quante volte sono verificati. stessa cosa succeda in resource strategy
 
     /**
      * this method will be recognized if the Resource cards
      * on the PlayerDesk meet the requirements of the objective card.
      * in this case it will analyze the objective that requires a specific sequence of resources
+     *
      * @param desk
      */
-    public int isSatisfied (PlayerDesk desk) {
+    public int isSatisfied(PlayerDesk desk) {
         EnumMap<Resource, Integer> TotalResources = desk.getTotalResources();
+        return TotalResources.get(resourceStrategyToCheck) / numOfResourcesToCheck;
     }
 
 
