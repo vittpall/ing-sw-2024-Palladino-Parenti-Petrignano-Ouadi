59fc753e1d8e2bfb5fe5ce0d8134305fe8ffaf36
Marouan Ouadi <ouadimarouan@gmail.com>
2024-04-20 18:32:45 +0200

    Added objectiveCard.json with some improvements

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ObjectiveCard.java
@@ -1,34 +1,35 @@
 package it.polimi.ingsw.model;
 
-import java.util.ArrayList;
-
 /**
  * Contains a reference to the possible objectives (strategies) in the game
  */
-public class ObjectiveCard extends Card{
+public class ObjectiveCard extends Card {
     ObjectiveStrategy strategy;
 
     public ObjectiveCard(ObjectiveCard copy) {
         this.strategy = copy.getStrategy();
-        this.points  = copy.getPoints();
+        this.points = copy.getPoints();
     }
 
     /**
      * Default constructor used to initialise the strategy reference
+     *
      * @param strategy it's the strategy we want to use
      */
-    public ObjectiveCard(ObjectiveStrategy strategy)
-    {
+    public ObjectiveCard(ObjectiveStrategy strategy, int points, String backImagePath, String frontImagePath) {
         this.strategy = strategy;
+        this.points = points;
+        this.backImagePath = backImagePath;
+        this.frontImagePath = frontImagePath;
     }
 
     /**
      * Will call the method isSatisfied which will make override on the concrete strategies
+     *
      * @param desk
      * @return the number of times the objective is verified
      */
-    public int verifyObjective(PlayerDesk desk)
-    {
+    public int verifyObjective(PlayerDesk desk) {
         return strategy.isSatisfied(desk);
     }
 
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/util/ObjectiveCardLoader.java
@@ -0,0 +1,31 @@
+package it.polimi.ingsw.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import it.polimi.ingsw.model.ObjectiveCard;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectiveCardLoader {
+
+
+    public List<ObjectiveCard> loadObjectiveCards() {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(module);
+        List<ObjectiveCard> cards = new ArrayList<>();
+        try {
+            cards = mapper.readValue(new File("src/main/resources/objectiveCard.json"), new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cards;
+    }
+}
