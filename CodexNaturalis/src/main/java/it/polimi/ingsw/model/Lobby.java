be357a66375947b3c23191e6f4fad7384db66ac3
Marouan Ouadi <ouadimarouan@gmail.com>
2024-04-27 09:35:23 +0200

    Created Lobby Model

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lobby {
+    private final Map<Integer, Game> games;
+    private int nextGameId;
+
+    public Lobby() {
+        games = new HashMap<>();
+        nextGameId = 1;
+    }
+
+
+    public void removeGame(int gameId) {
+        games.remove(gameId);
+    }
+
+    public Game getGame(int gameId) {
+        return games.get(gameId);
+    }
+
+    public Map<Integer, Game> listAllGames() {
+        return new HashMap<>(games);
+    }
+
+    public Game createNewGame(int numberOfPlayers) {
+        Game newGame = new Game(nextGameId, numberOfPlayers);
+        games.put(nextGameId, newGame);
+        nextGameId++;
+        return newGame;
+    }
+
+    public boolean joinGame(int gameId, Player player) {
+        Game game = getGame(gameId);
+        game.addPlayer(player);
+        return true;
+    }
+
+}
