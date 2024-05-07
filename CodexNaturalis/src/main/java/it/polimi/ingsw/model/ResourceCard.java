79475aa843586fdeb5827c18e9a0ad40eae79f57
Marouan Ouadi <ouadimarouan@gmail.com>
2024-05-07 11:42:22 +0200

    Don't show resourceCard points if zero

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/ResourceCard.java
@@ -44,8 +44,12 @@ public class ResourceCard extends GameCard {
 
         for (int i = 0; i < context.getCardHeight() - 2; i++) {
             if (i == context.getCardHeight() / 2 - 1) {
-                String pointsDetail = "Pts: " + getPoints();
-                System.out.println(colorBackground + " " + context.centerString(pointsDetail, context.getCardWidth() - 2) + " " + CardPrinter.RESET);
+                if (getPoints() != 0) {
+                    String pointsDetail = "Pts: " + getPoints();
+                    System.out.println(colorBackground + " " + context.centerString(pointsDetail, context.getCardWidth() - 2) + " " + CardPrinter.RESET);
+                } else {
+                    System.out.println(colorBackground + context.centerString("", context.getCardWidth()) + CardPrinter.RESET);
+                }
             } else {
                 System.out.println(colorBackground + context.repeat(context.getCardWidth()) + CardPrinter.RESET);
             }
