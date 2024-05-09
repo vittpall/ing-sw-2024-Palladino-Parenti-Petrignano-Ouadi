8482480832fc18c9b4c9206641168e2190311834
Marouan Ouadi <ouadimarouan@gmail.com>
2024-05-09 22:46:40 +0200

    Removed unnecessary System.out.print

--- CodexNaturalis/src/main/java/it/polimi/ingsw/tui/ColorSelection.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/tui/ColorSelection.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.tui;
 import it.polimi.ingsw.model.enumeration.TokenColor;
 import it.polimi.ingsw.network.RemoteInterfaces.VirtualView;
 
-import javax.swing.*;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -15,7 +14,6 @@ public class ColorSelection implements ClientState {
 
     @Override
     public void promptForInput() {
-        System.out.print("Enter your choice: Select an available color:\n 1. RED\n 2. BLUE\n 3. YELLOW\n 4. GREEN\n: ");
     }
 
     public ColorSelection(VirtualView client, Scanner scanner) {
@@ -24,19 +22,19 @@ public class ColorSelection implements ClientState {
     }
 
     @Override
-    public void display () {
+    public void display() {
         int i;
         System.out.println("\n ----------Choose a color----------");
         System.out.println("Available colors: \n");
-        try{
-           ArrayList<TokenColor> ColorList = client.getAvailableColors();
-           for(i=0; i<ColorList.size(); i++){
-               System.out.println(ColorList.get(i) + "\n");
-           }
-       }catch(RemoteException ex){
-           System.out.println("\nRemote exception\n"+ex.getMessage());
-       }
-       System.out.println("--------------------------------\n");
+        try {
+            ArrayList<TokenColor> ColorList = client.getAvailableColors();
+            for (i = 0; i < ColorList.size(); i++) {
+                System.out.println(ColorList.get(i) + "\n");
+            }
+        } catch (RemoteException ex) {
+            System.out.println("\nRemote exception\n" + ex.getMessage());
+        }
+        System.out.println("--------------------------------\n");
     }
 
     @Override
@@ -44,34 +42,34 @@ public class ColorSelection implements ClientState {
         ArrayList<TokenColor> ColorList = client.getAvailableColors();
         switch (input) {
             case 1:
-                if(ColorList.contains(TokenColor.RED)) {
+                if (ColorList.contains(TokenColor.RED)) {
                     client.setTokenColor(TokenColor.RED);
                     client.setCurrentState(new InitializeObjectiveCardState(client, scanner));
-                }else
+                } else
                     System.out.println("Color not available\n");
 
                 break;
             case 2:
-                if(ColorList.contains(TokenColor.BLUE)) {
+                if (ColorList.contains(TokenColor.BLUE)) {
                     client.setTokenColor(TokenColor.BLUE);
                     client.setCurrentState(new InitializeObjectiveCardState(client, scanner));
-                }else
+                } else
                     System.out.println("Color not available\n");
                 break;
 
             case 3:
-                if(ColorList.contains(TokenColor.YELLOW)) {
+                if (ColorList.contains(TokenColor.YELLOW)) {
                     client.setTokenColor(TokenColor.YELLOW);
                     client.setCurrentState(new InitializeObjectiveCardState(client, scanner));
-                }else
+                } else
                     System.out.println("Color not available\n");
                 break;
 
             case 4:
-                if(ColorList.contains(TokenColor.GREEN)) {
+                if (ColorList.contains(TokenColor.GREEN)) {
                     client.setTokenColor(TokenColor.GREEN);
                     client.setCurrentState(new InitializeObjectiveCardState(client, scanner));
-                }else
+                } else
                     System.out.println("Color not available\n");
                 break;
 
