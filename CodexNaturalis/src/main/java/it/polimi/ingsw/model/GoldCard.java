9cbd5b5a7aeb8d843c73beabcd06eca22bfc5410
Marouan Ouadi <ouadimarouan@gmail.com>
2024-05-12 22:05:47 +0200

    Fixed printCardDetails of GoldCards

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/GoldCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/GoldCard.java
@@ -59,23 +59,17 @@ public class GoldCard extends GameCard {
     @Override
     protected void printCardDetails(PrintContext context, CardPrinter.Color colorBackground, boolean faceDown) {
         for (int i = 0; i < context.getCardHeight() - 2; i++) {
-            if (faceDown) {
-                if (i == context.getCardHeight() / 2 - 1) {
-                    String pointsDetail = "Pts: " + getPoints() + " " + getPointType();
-                    System.out.println(colorBackground + " " + context.centerString(pointsDetail, context.getCardWidth() - 2) + " " + CardPrinter.RESET);
-                    // Handling requirements, format them to show only the first character of each resource name
-                    if (!getRequirements().isEmpty()) {
-                        StringBuilder reqDetails = new StringBuilder("Req: ");
-                        getRequirements().forEach((resource, quantity) -> {
-                            String resourceInitial = resource.name().substring(0, 1); // Get first character of the resource name
-                            reqDetails.append(resourceInitial).append("=").append(quantity).append(", ");
-                        });
-                        // Remove the last comma and space
-                        reqDetails.setLength(reqDetails.length() - 2);
-                        System.out.println(colorBackground + " " + context.centerString(reqDetails.toString(), context.getCardWidth() - 2) + " " + CardPrinter.RESET);
-                    }
-
-                }
+            if (!faceDown && (i == context.getCardHeight() / 2 - 1) && getPoints() != 0) {
+                String pointsDetail = "Pts: " + getPoints() + " " + getPointType();
+                System.out.println(colorBackground + " " + context.centerString(pointsDetail, context.getCardWidth() - 2) + " " + CardPrinter.RESET);
+                // Handling requirements, format them to show only the first character of each resource name
+                StringBuilder reqDetails = new StringBuilder("Req: ");
+                getRequirements().forEach((resource, quantity) -> {
+                    String resourceInitial = resource.name().substring(0, 1); // Get first character of the resource name
+                    reqDetails.append(resourceInitial).append("=").append(quantity).append(", ");
+                });
+                reqDetails.setLength(reqDetails.length() - 2);
+                System.out.println(colorBackground + " " + context.centerString(reqDetails.toString(), context.getCardWidth() - 2) + " " + CardPrinter.RESET);
             } else {
                 System.out.println(colorBackground + context.repeat(context.getCardWidth()) + CardPrinter.RESET);
             }
