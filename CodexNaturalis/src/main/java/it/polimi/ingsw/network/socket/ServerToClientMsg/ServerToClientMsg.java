1cef2fcfab09d53da5c2a6fe188fc04d30e98dbd
Marouan Ouadi <ouadimarouan@gmail.com>
2024-05-31 11:52:04 +0200

    Removed unnecessary doItNeedToBeBroadCasted attribute from ServerToClientMsg

--- CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/ServerToClientMsg/ServerToClientMsg.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/network/socket/ServerToClientMsg/ServerToClientMsg.java
@@ -4,68 +4,49 @@ import it.polimi.ingsw.model.enumeration.TypeServerToClientMsg;
 import it.polimi.ingsw.network.socket.Client.ReturnableObject;
 
 import java.io.Serializable;
-import java.lang.reflect.Type;
 
 
 public class ServerToClientMsg implements Serializable {
 
     protected ReturnableObject response;
     protected TypeServerToClientMsg type;
-    protected boolean doItNeedToBeBroadCasted;
     protected int idGame;
 
     public ServerToClientMsg(TypeServerToClientMsg type) {
         this.type = type;
-        this.doItNeedToBeBroadCasted = false;
         this.response = new ReturnableObject<>();
     }
 
-    public ServerToClientMsg(TypeServerToClientMsg type, boolean doItNeedToBeBroadCasted) {
-        this.type = type;
-        this.doItNeedToBeBroadCasted = doItNeedToBeBroadCasted;
-        this.response = new ReturnableObject<>();
-    }
 
-    public ServerToClientMsg(TypeServerToClientMsg type, boolean doItNeedToBeBroadCasted, int idGame) {
+    public ServerToClientMsg(TypeServerToClientMsg type, int idGame) {
         this.type = type;
-        this.doItNeedToBeBroadCasted = doItNeedToBeBroadCasted;
         this.response = new ReturnableObject<>();
         this.idGame = idGame;
     }
 
-    public void setResponse(ReturnableObject response)
-    {
+    public void setResponse(ReturnableObject response) {
         this.response = response;
     }
 
-    public ReturnableObject getResponse()
-    {
+    public ReturnableObject getResponse() {
         return this.response;
     }
 
-    public void setType(TypeServerToClientMsg type)
-    {
+    public void setType(TypeServerToClientMsg type) {
         this.type = type;
     }
 
-    public TypeServerToClientMsg getType()
-    {
+    public TypeServerToClientMsg getType() {
         return this.type;
     }
 
-    public void setIdGame(int idGame)
-    {
+    public void setIdGame(int idGame) {
         this.idGame = idGame;
     }
 
-    public int getIdGame()
-    {
+    public int getIdGame() {
         return this.idGame;
     }
 
-    public boolean doItNeedToBeBroadCasted()
-    {
-        return this.doItNeedToBeBroadCasted;
-    }
 
 }
