7f1502706db186dbb2a8b52159a34e2314612f50
Marouan Ouadi <ouadimarouan@gmail.com>
2024-05-31 20:49:02 +0200

    Replaced ListView in ColorSelection.fxml with Token images

--- CodexNaturalis/src/main/java/it/polimi/ingsw/gui/Controller/ColorSelectionController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/gui/Controller/ColorSelectionController.java
@@ -3,71 +3,58 @@ package it.polimi.ingsw.gui.Controller;
 import it.polimi.ingsw.gui.ObjectiveCardSelectionStateGUI;
 import it.polimi.ingsw.model.enumeration.TokenColor;
 import it.polimi.ingsw.network.BaseClient;
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ColorSelectionController implements FXMLController {
+    public HBox colorContainer;
+    public Label messageLabel;
+    public Button selectColorButton;
     private Stage stage;
-    @FXML
-    private ListView<TokenColor> colorListView;
-    @FXML
-    private Label messageLabel;
-    @FXML
-    private Button selectColorButton;
 
     private BaseClient client;
-
+    private TokenColor selectedColor;
 
     @FXML
     private void initialize() {
         selectColorButton.setDisable(true);
-        colorListView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> selectColorButton.setDisable(newSelection == null));
     }
 
     public void updateColorList() {
         try {
             ArrayList<TokenColor> colorList = client.getAvailableColors();
+            colorContainer.getChildren().clear();
+            for (TokenColor color : colorList) {
+                ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/" + color.getImageName()))));
+                imageView.setFitHeight(100);
+                imageView.setFitWidth(100);
+                imageView.setPreserveRatio(true);
 
-            if (colorList.isEmpty()) {
-                messageLabel.setText("No colors available.");
-                selectColorButton.setDisable(true);
-            } else {
-                colorListView.setItems(FXCollections.observableArrayList(colorList));
-                colorListView.setCellFactory(lv -> new ListCell<>() {
-                    @Override
-                    protected void updateItem(TokenColor color, boolean empty) {
-                        super.updateItem(color, empty);
-                        if (empty || color == null) {
-                            setText(null);
-                            setGraphic(null);
-                        } else {
-                            Circle colorCircle = new Circle(10, colorMap(color));
-                            setText(color.name());
-                            setGraphic(colorCircle);
-                        }
-                    }
+                imageView.setOnMouseClicked(event -> {
+                    selectedColor = color;
+                    selectColorButton.setDisable(false);
                 });
+
+                colorContainer.getChildren().add(imageView);
             }
+
         } catch (IOException | InterruptedException e) {
             messageLabel.setText("Error updating color list: " + e.getMessage());
         }
     }
 
-    //TODO here the listener will recall the method display to refresh the list of colors.
+    @FXML
     public void handleSelectColor() throws IOException, InterruptedException {
-        TokenColor selectedColor = colorListView.getSelectionModel().getSelectedItem();
         if (selectedColor != null && client.getAvailableColors().contains(selectedColor)) {
             try {
                 client.setTokenColor(selectedColor);
@@ -83,14 +70,6 @@ public class ColorSelectionController implements FXMLController {
         }
     }
 
-    private Paint colorMap(TokenColor color) {
-        return switch (color) {
-            case RED -> Color.RED;
-            case BLUE -> Color.BLUE;
-            case YELLOW -> Color.YELLOW;
-            case GREEN -> Color.GREEN;
-        };
-    }
 
     public void setClient(BaseClient client) {
         this.client = client;
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/enumeration/TokenColor.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/enumeration/TokenColor.java
@@ -8,25 +8,38 @@ import java.io.Serializable;
  * @author Carolina Parenti
  */
 public enum TokenColor implements Serializable {
-    RED("Red"), BLUE("Blue"), YELLOW("Yellow"), GREEN("Green");
+
+    RED("Red", "CODEX_pion_rouge.png"),
+    BLUE("Blue", "CODEX_pion_bleu.png"),
+    YELLOW("Yellow", "CODEX_pion_jaune.png"),
+    GREEN("Green", "CODEX_pion_vert.png");
 
     private final String value;
+    private final String imageName;
 
     /**
      * constructor
      *
-     * @param value represents the name of the TokenColor
+     * @param value     represents the name of the TokenColor
+     * @param imageName represents the image file name of the TokenColor
      */
-    TokenColor(String value) {
+    TokenColor(String value, String imageName) {
         this.value = value;
+        this.imageName = imageName;
     }
 
     /**
      * Returns the value of the string representing the TokenColor
      */
-
     public String get() {
         return value;
+    }
+
+    /**
+     * Returns the image name representing the TokenColor
+     */
+    public String getImageName() {
+        return imageName;
     }
 
     @Override
