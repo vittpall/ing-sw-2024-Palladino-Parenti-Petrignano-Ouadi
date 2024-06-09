5345c442ae99da252a27a10796604d65fa6082c7
Marouan Ouadi <ouadimarouan@gmail.com>
2024-06-09 14:42:45 +0200

    Show start game button only when necessary

--- CodexNaturalis/src/main/java/it/polimi/ingsw/gui/Controller/WaitingForPlayersController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/gui/Controller/WaitingForPlayersController.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.gui.Controller;
 
 import it.polimi.ingsw.gui.ColorSelectionGUI;
-import it.polimi.ingsw.network.BaseClient;
 import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.network.BaseClient;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WaitingForPlayersController implements FXMLController {
+    public Button StartGameButton;
     private BaseClient client;
     private Stage stage;
     @FXML
@@ -30,7 +32,7 @@ public class WaitingForPlayersController implements FXMLController {
         try {
             ArrayList<Player> players = client.getPlayers(client.getIdGame());
             int size = players.size();
-            switch(size){
+            switch (size) {
                 case 0:
                     player1Info.setVisible(false);
                     player2Info.setVisible(false);
@@ -38,33 +40,33 @@ public class WaitingForPlayersController implements FXMLController {
                     player4Info.setVisible(false);
                     break;
                 case 1:
-                    player1Info.setText("Player 1 : "+players.getFirst().getUsername());
+                    player1Info.setText("Player 1 : " + players.getFirst().getUsername());
                     player2Info.setVisible(false);
                     player3Info.setVisible(false);
                     player4Info.setVisible(false);
                     break;
                 case 2:
-                    player1Info.setText("Player 1 : "+players.get(0).getUsername());
-                    player2Info.setText("Player 2 : "+players.get(1).getUsername());
+                    player1Info.setText("Player 1 : " + players.get(0).getUsername());
+                    player2Info.setText("Player 2 : " + players.get(1).getUsername());
                     player3Info.setVisible(false);
                     player4Info.setVisible(false);
                     break;
                 case 3:
-                    player1Info.setText("Player 1 : "+players.get(0).getUsername());
-                    player2Info.setText("Player 2 : "+players.get(1).getUsername());
-                    player3Info.setText("Player 3 : "+players.get(2).getUsername());
+                    player1Info.setText("Player 1 : " + players.get(0).getUsername());
+                    player2Info.setText("Player 2 : " + players.get(1).getUsername());
+                    player3Info.setText("Player 3 : " + players.get(2).getUsername());
                     player4Info.setVisible(false);
                     break;
                 case 4:
-                    player1Info.setText("Player 1 : "+players.get(0).getUsername());
-                    player2Info.setText("Player 2 : "+players.get(1).getUsername());
-                    player3Info.setText("Player 3 : "+players.get(2).getUsername());
-                    player4Info.setText("Player 4 : "+players.get(3).getUsername());
+                    player1Info.setText("Player 1 : " + players.get(0).getUsername());
+                    player2Info.setText("Player 2 : " + players.get(1).getUsername());
+                    player3Info.setText("Player 3 : " + players.get(2).getUsername());
+                    player4Info.setText("Player 4 : " + players.get(3).getUsername());
                     break;
             }
             if (!(client.getnPlayer(client.getIdGame()) > size)) {
                 waitingForNPlayersLabel.setText("You can start the game");
-            }else
+            } else
                 waitingForNPlayersLabel.setText("Waiting for " + (client.getnPlayer(client.getIdGame()) - size) + " players to join the game...");
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -84,28 +86,35 @@ public class WaitingForPlayersController implements FXMLController {
 
     public void handleServerNotification(String msg) {
         try {
-            switch(client.getPlayers(client.getIdGame()).size()){
+            int currentPlayers = client.getPlayers(client.getIdGame()).size();
+            int totalNeededPlayers = client.getnPlayer(client.getIdGame());
+
+            switch (currentPlayers) {
                 case 2:
                     player2Info.setText(msg);
                     player2Info.setVisible(true);
-                    waitingForNPlayersLabel.setText("Waiting for " + (client.getnPlayer(client.getIdGame()) - client.getPlayers(client.getIdGame()).size()) + " players to join the game...");
                     break;
                 case 3:
                     player3Info.setText(msg);
                     player3Info.setVisible(true);
-                    waitingForNPlayersLabel.setText("Waiting for " + (client.getnPlayer(client.getIdGame()) - client.getPlayers(client.getIdGame()).size()) + " players to join the game...");
                     break;
                 case 4:
                     player4Info.setText(msg);
                     player4Info.setVisible(true);
-                    waitingForNPlayersLabel.setText("Waiting for " + (client.getnPlayer(client.getIdGame()) - client.getPlayers(client.getIdGame()).size()) + " players to join the game...");
                     break;
                 default:
                     break;
             }
-            if (client.getnPlayer(client.getIdGame()) == client.getPlayers(client.getIdGame()).size()) {
+
+            waitingForNPlayersLabel.setText("Waiting for " + (totalNeededPlayers - currentPlayers) + " players to join the game...");
+
+            if (currentPlayers == totalNeededPlayers) {
                 waitingForNPlayersLabel.setText("You can start the game");
+                StartGameButton.setVisible(true);
+            } else {
+                StartGameButton.setVisible(false);
             }
+
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
