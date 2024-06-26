ca4b0e28b7e3bcd9602d91711d356b7c4b730cda
Marouan Ouadi <ouadimarouan@gmail.com>
2024-06-27 00:14:04 +0200

    Added Application logo

--- CodexNaturalis/src/main/java/it/polimi/ingsw/util/FXMLLoaderUtility.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/util/FXMLLoaderUtility.java
@@ -1,11 +1,14 @@
 package it.polimi.ingsw.util;
 
-import it.polimi.ingsw.view.gui.controller.FXMLController;
 import it.polimi.ingsw.network.BaseClient;
+import it.polimi.ingsw.view.gui.controller.FXMLController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class FXMLLoaderUtility {
 
@@ -13,11 +16,11 @@ public class FXMLLoaderUtility {
      * this method loads the view of the FXML file.
      * It sets the client and the new stage of the controller.
      *
-     * @param stage     is the new stage of the controller
-     * @param client    is the client that communicates with the server
-     * @param fxmlPath  is the path of the FXML file
-     * @return          the controller of the FXML file
-     * @param <T>       is the type of the controller
+     * @param stage    is the new stage of the controller
+     * @param client   is the client that communicates with the server
+     * @param fxmlPath is the path of the FXML file
+     * @param <T>      is the type of the controller
+     * @return the controller of the FXML file
      */
     public static <T extends FXMLController> T loadView(Stage stage, BaseClient client, String fxmlPath) {
         try {
@@ -29,6 +32,9 @@ public class FXMLLoaderUtility {
             controller.setStage(stage);
 
             Scene scene = new Scene(root);
+
+            Image icon = new Image(Objects.requireNonNull(FXMLLoaderUtility.class.getResourceAsStream("/Images/CodexLogo.jpg")));
+            stage.getIcons().add(icon);
 
             stage.setScene(scene);
             stage.show();
